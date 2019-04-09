2d8bb9d3dcb93482f5070de75d3814f541bc205e
sx2333 <374495727@qq.com>
2019-04-09 17:33:54 +0800

    4.9 summarize

--- ex2-21/ex4.py
+++ ex2-21/ex4.py
@@ -7,7 +7,7 @@ cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car #声明总计可载人数
 average_passengers_per_car = passengers / cars_driven #声明每辆车的平均载人数
 
-print("There are",cars,"cars available.")   
+print("There are"+ cars +"cars available.")   
 print("There are only",drivers,"drivers available.")
 print("There will be",cars_not_driven,"empty cars today.")
 print("We can transport",carpool_capacity,"people today.")
